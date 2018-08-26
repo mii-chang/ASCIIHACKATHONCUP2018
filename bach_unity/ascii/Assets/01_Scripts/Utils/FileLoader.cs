2c974836f31ffbe583fd1036569bee35ab7b24f1
kotouno yuta <bananakotono@gmail.com>
2018-08-26 14:34:11 +0900

    add getAngle

--- bach_unity/ascii/Assets/01_Scripts/Utils/FileLoader.cs
+++ bach_unity/ascii/Assets/01_Scripts/Utils/FileLoader.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System.Linq;
+using System;
 
 public class FileLoader : MonoBehaviour {
 
@@ -12,13 +13,28 @@ public class FileLoader : MonoBehaviour {
     public Text[] user1Text;
     public GameObject[] user2TextObjs;
     public Text[] user2Text;
+    static float x;
+    static float y;
+    List<Vector2> player1 = new List<Vector2>();
+    List<Vector2> player2 = new List<Vector2>();
 
     private void Awake() {
-        user1TextObjs = GameObject.FindGameObjectsWithTag("user1_texts");
-        user1Text = user1TextObjs.Select(obj => obj.GetComponent<Text>()).Reverse().ToArray();
+        //user1TextObjs = GameObject.FindGameObjectsWithTag("user1_texts");
+        //user1Text = user1TextObjs.Select(obj => obj.GetComponent<Text>()).Reverse().ToArray();
 
-        user2TextObjs = GameObject.FindGameObjectsWithTag("user2_texts");
-        user2Text = user2TextObjs.Select(obj => obj.GetComponent<Text>()).Reverse().ToArray();
+        //user2TextObjs = GameObject.FindGameObjectsWithTag("user2_texts");
+        //user2Text = user2TextObjs.Select(obj => obj.GetComponent<Text>()).Reverse().ToArray();
+    }
+
+    public float GetAngle(Vector2 basic_point, Vector2 point1, Vector2 point2){
+        double direction1_x  = point1.x - basic_point.x;
+        double direction1_y  = point1.y - basic_point.y;
+        double direction2_x  = point2.x - basic_point.x;
+        double direction2_y  = point2.y - basic_point.y;
+
+        double cos = (direction1_x * direction2_x + direction1_y * direction2_y) / Math.Sqrt(Math.Pow(direction1_x, 2) + Math.Pow(direction1_y, 2)) / Math.Sqrt(Math.Pow(direction2_x, 2) + Math.Pow(direction2_y, 2));
+        double radius = Math.Acos(cos) * 180 / Math.PI;
+        return (float)radius;
     }
 
 
@@ -26,38 +42,69 @@ public class FileLoader : MonoBehaviour {
         return JsonUtility.FromJson<OpenPose>(json);
     }
 
-    public void DrawOpenPoseData(OpenPose data) {
+    public void DrawOpenPoseData(OpenPose data)
+    {
         int index = 0;
         if (data.people.Count == 0) return;
         data.people[0].pose_keypoints.ForEach(points =>
         {
-            if (index % 3 == 0) {
+            if (index % 3 == 0)
+            {
                 tempMes += points.ToString("F1") + ", ";
-            } else if (index % 3 == 1) {
+                x = points;
+            }
+            else if (index % 3 == 1)
+            {
                 tempMes += points.ToString("F1") + "]";
+                y = points;
             }
 
-            if (index % 3 == 2) {
+            if (index % 3 == 2)
+            {
+                player1.Add(new Vector2(x, y));
                 user1Text[index / 3].text = tempMes;
                 tempMes = "[";
             }
             index++;
         });
         if (data.people.Count == 1) return;
-        data.people[1].pose_keypoints.ForEach(points =>
+        index = 0;
+
+        data.people[2].pose_keypoints.ForEach(points =>
         {
-            if (index % 3 == 0) {
+            if (index % 3 == 0)
+            {
                 tempMes += points.ToString("F1") + ", ";
-            } else if (index % 3 == 1) {
+                x = points;
+            }
+            else if (index % 3 == 1)
+            {
                 tempMes += points.ToString("F1") + "]";
+                y = points;
             }
 
-            if (index % 3 == 2) {
+            if (index % 3 == 2)
+            {
+                player2.Add(new Vector2(x, y));
                 user2Text[index / 3].text = tempMes;
                 tempMes = "[";
             }
             index++;
         });
-    }
 
+        float center_x = (player1[11][0] - player1[8][0]) / 2 + player1[8][0];
+        float center_y = (player1[11][1] - player1[8][1]) / 2 + player1[8][1];
+        Vector2 center = new Vector2(
+            center_x, center_y
+        );
+
+        float leg_radius = GetAngle(center, player1[10], player1[13]);
+        float leg_point  = leg_radius * 100 / 180;
+
+        float arm_radius = GetAngle(player1[1], player1[4], player1[7]);
+        float arm_point  = 100 - Math.Abs(120 - arm_radius);
+
+        print(leg_point);
+        print(arm_point);
+    }
 }
