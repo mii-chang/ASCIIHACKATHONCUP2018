6bfc5cf8e91d0f4b9383c4ff8d90dd0ffd9a76e3
ryosebach <ryo.freezia@gmail.com>
2018-09-05 09:07:38 +0900

    add ScoreManager

---
+++ bach_unity/ascii/Assets/01_Scripts/Manager/ScoreManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+
+    public ScoreData team1ScoreData { get; private set; }
+    public ScoreData team2ScoreData { get; private set; }
+
+    private void Start() {
+
+    }
+}
+
+public class ScoreData {
+    public int maxCombo;
+    public int score;
+    public int perfectCount;
+    public int missCount;
+}
